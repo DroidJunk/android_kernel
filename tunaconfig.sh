6491e55959d9e9218fbe18b9ca29e34f8e961fe7
Jimmy Miller <jbmiller148@gmail.com>
2012-04-03 19:47:35 -0500

    Script for easy build

--- tunaconfig.sh
+++ tunaconfig.sh
@@ -1,5 +1,5 @@
 export ARCH=arm
 make tuna_defconfig
-export CROSS_COMPILE=/media/Disc1/android/ics43/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+export CROSS_COMPILE=/media/Disc1/android/ics44/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 make menuconfig
-make -j32 ARCH=arm CROSS_COMPILE=/media/Disc1/android/ics43/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+make -j32 ARCH=arm CROSS_COMPILE=/media/Disc1/android/ics44/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
